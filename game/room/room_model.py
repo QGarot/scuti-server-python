a84f0ac80622b441d627f9e8e4586348af987ebe
QGarot <quentingarot06@gmail.com>
2022-08-30 20:34:35 +0200

    room loading fixed

--- game/room/room_model.py
+++ game/room/room_model.py
@@ -1,9 +1,11 @@
+from utils.array import Matrix
+
 OPEN = 0
 CLOSED = 1
 
 
 class RoomModel:
-    def __init__(self, name, heightmap, door_x, door_y, door_z, door_rotation):
+    def __init__(self, name: str, heightmap: str, door_x: int, door_y: int, door_z: float, door_rotation: int):
         self.name = name
         self.heightmap = heightmap
         self.door_x = door_x
@@ -11,53 +13,41 @@ class RoomModel:
         self.door_z = door_z
         self.door_rotation = door_rotation
 
-        temporary = self.heightmap.split("{13}")
+        self.map_size_x = None
+        self.map_size_y = None
+        self.squares = None
+        self.square_height = None
+        self.generate_heightmap_lookups()
 
+        self.floor_map = None
+        self.generate_relative_heightmap()
+
+    def generate_heightmap_lookups(self):
+        temporary = self.heightmap.split("{13}")
         self.map_size_x = len(temporary[0])
         self.map_size_y = len(temporary)
 
-        self.squares = self.get_2d_array(CLOSED)
-        self.square_height = self.get_2d_array(CLOSED)
-        self.square_char = self.get_2d_array(CLOSED)
+        self.squares = Matrix(self.map_size_x, self.map_size_y, 0)
+        self.square_height = Matrix(self.map_size_x, self.map_size_y, 0)
 
-        for y in range(0, self.map_size_y):
-
-            # if y > 0:
-            #    temporary[y] = temporary[y][1:] # Substring 1
-
-            for x in range(0, self.map_size_x):
-                square = temporary[y][x:x + 1].strip().lower()
-
+        for y in range(self.map_size_y):
+            line = temporary[y].replace(chr(10), "").replace(chr(13), "")
+            x = 0
+            for square in line:
                 if square == "x":
-                    self.squares.append(x)
-                    self.squares[x][y] = CLOSED
+                    self.squares.set(x, y, CLOSED)
+                else:
+                    self.squares.set(x, y, OPEN)
+                    self.square_height.set(x, y, RoomModel.parse(square))
 
-                elif self.is_numeric(square):
-                    self.squares[x][y] = OPEN
-                    self.square_height[x][y] = float(RoomModel.parse(square))
+                if x == self.door_x and y == self.door_y:
+                    self.squares.set(x, y, OPEN)
+                    self.square_height.set(x, y, self.door_z)
 
-                if self.door_x == x and self.door_y == y:
-                    self.squares[x][y] = OPEN
-                    self.square_height[x][y] = float(self.door_z)
+                x = x + 1
 
-                self.square_char[x][y] = square
-
-        string_builder = ""
-
-        for y in range(0, self.map_size_y):
-            for x in range(0, self.map_size_x):
-
-                try:
-                    if x == self.door_x and y == self.door_y:
-                        string_builder += str(self.door_z)
-                    else:
-                        string_builder += self.square_char[x][y]
-                except Exception as e:
-                    string_builder += "0"
-
-            string_builder += chr(13)
-
-        self.floor_map = string_builder
+    def generate_relative_heightmap(self):
+        self.floor_map = self.heightmap.replace("{13}", chr(13))
 
     @staticmethod
     def parse(square):
